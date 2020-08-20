1ccd82b43f4821a3f41780d5396555e626162d17
TOluwoleSparta <69895618+TOluwoleSparta@users.noreply.github.com>
2020-08-20 08:44:13 +0100

    Added Printer package/class
    Printer class contains the following methods
    
    - printQuotes : prints with double quotes around the statement
    - printNewLine : prints with a new line after the statement

---
+++ PalindromeProject/src/com/sparta/printer/Printer.java
@@ -0,0 +1,13 @@
+package com.sparta.printer;
+
+public class Printer {
+
+
+    public static void printQuotes(Object outputToPrint) {
+        System.out.println("\"" + outputToPrint + "\"");
+    }
+
+    public static void printNewLine(Object outputToPrint) {
+        System.out.println(outputToPrint + "\n");
+    }
+}
