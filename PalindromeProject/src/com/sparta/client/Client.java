f5e5be49131548a8d033e7fc7e6f418e3fdb20f7
TOluwoleSparta <69895618+TOluwoleSparta@users.noreply.github.com>
2020-08-20 08:49:09 +0100

    Updated Client (full names/printer)
    Updated Client class to 
    
    - check if full names are palindromes
    - print return statements using the Printer class instead of formatted System.out.println() statements

--- PalindromeProject/src/com/sparta/client/Client.java
+++ PalindromeProject/src/com/sparta/client/Client.java
@@ -2,9 +2,12 @@ package com.sparta.client;
 
 import com.sparta.PalindromeChecker;
 import com.sparta.PalindromeSentence;
+import com.sparta.printer.Printer;
 
 public class Client {
     private String[] names = {"Hannah", "Anna", "David", "Alexa", "Elle", "Alli", "Pip"};
+
+    private String[] fullNames = {"Lily Lil", "Tomas Samot", "Paco Cap", "Liam Mail", "Ian D Nai"};
 
     private String[] sentences = {  "A kayak was caught on my radar",
                                     "I always noticed how at noon the sun is so yellow but during the evening it's a lot redder",
@@ -17,18 +20,26 @@ public class Client {
     public void runProgams() throws Exception {
         boolean isPalindrome;
         String longestPalindrome;
-
+        // names
         for (String name:names) {
             isPalindrome = PalindromeChecker.isWordAPalindrome(name);
 
-            System.out.println(name + "\n" + isPalindrome + "\n");
+            Printer.printQuotes(name);
+            Printer.printNewLine(isPalindrome);
         }
+        // full names
+        for (String name:fullNames) {
+            isPalindrome = PalindromeChecker.isWordAPalindrome(name);
 
-
+            Printer.printQuotes(name);
+            Printer.printNewLine(isPalindrome);
+        }
+        // sentences
         for (String sentence:sentences) {
             longestPalindrome = PalindromeSentence.getLongestPalindrome(sentence);
 
-            System.out.println(sentence + "\n" + longestPalindrome + "\n");
+            Printer.printQuotes(sentence);
+            Printer.printNewLine(longestPalindrome);
         }
     }
 }
